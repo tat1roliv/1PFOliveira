6b92a0c3f149c4fef80a5b94f197ebc4d9e268e9
tati oliveira <tat1roliv@gmail.com>
2023-02-06 20:09:01 -0300

    add table data + table angular material

--- demo/src/app/components/students-list/students-list.component.ts
+++ demo/src/app/components/students-list/students-list.component.ts
@@ -1,10 +1,64 @@
 import { Component } from '@angular/core';
+import { Student } from '../../models/student';
+
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-students-list',
   templateUrl: './students-list.component.html',
   styleUrls: ['./students-list.component.css']
 })
+
+
 export class StudentsListComponent {
+
+
+students: Student[] = [
+    {
+      name: 'Ada',
+      lastName: 'Lovelace',
+      email: 'ada@test.com',
+      course: 'Angular',
+      isActive: true,
+    },
+    {
+      name: 'Alan',
+      lastName: 'Turing',
+      email: 'a.turing@test.com',
+      course: 'C',
+      isActive: true,
+    },
+    {
+      name: 'Robert',
+      lastName: 'Martin',
+      email: 'unclebob@test.com',
+      course: 'Java',
+      isActive: true,
+    },
+    {
+      name: 'Tim',
+      lastName: 'Berners-Lee ',
+      email: 'tim@test.com',
+      course: 'React',
+      isActive: false,
+    },
+    {
+      name: 'Grace',
+      lastName: 'Hopper',
+      email: 'hopper@test.com',
+      course: 'Cobol',
+      isActive: true,
+    },
+
+
+  ];
+
+  //instance table data / angular material
+  dataSource: MatTableDataSource<Student> = new MatTableDataSource<Student>(this.students);
+
+  //table cols header data
+  tableColumns: string[] = [ 'name' , 'lastName' , 'email' , 'course', 'isActive' , 'actions'];
+
+  //constructor(private dialog: MatDialog){}
 
 }
